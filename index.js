f4b3f4fdeb7ce6a07902eea8a0e98046680ba271
Diego Cotte <diegocotte1407@gmail.com>
2022-08-19 19:59:04 -0500

    Deploy

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routes');
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.com', 'http://127.0.0.1:5500'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  }
+}
+app.use(cors(options));
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/home', (req, res) => {
+  res.send('Aquí encontrarás la página principal');
+});
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log('Mi port ' + port);
+});
---
+++ routes/products.router.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const ProductsService = require('./../services/product.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createProductSchema, updateProductSchema, getProductSchema } = require('./../schemas/product.schema');
+
+const router = express.Router();
+const service = new ProductsService();
+
+router.get('/', async (req, res) => {
+  const products = await service.find();
+  res.json(products);
+});
+
+// Los endpoints específicos deben declararse antes de los endpoints dinámicos
+
+router.get('/filter', (req, res) => {
+  res.send('Yo soy un filtro');
+});
+
+// Si los endpoints dinámicos son declarados antes que los estáticos generaremos problemas de routing
+
+router.get('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = await service.findOne(id);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
+    const body = req.body;
+    const newProduct = await service.create(body);
+    res.status(201).json(newProduct);
+  }
+);
+
+// Se puede utilizar PUT o PATCH para hacer los updates...
+// Usamos PATCH para cambios parciales y PUT para cambios totales
+
+router.patch('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const product = await service.update(id, body);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const rta = await service.delete(id);
+  res.json(rta);
+});
+
+module.exports = router;
