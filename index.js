1a014519340a78dcc711a5a70c5aa6944846ab3e
Diego Cotte <diegocotte1407@gmail.com>
2022-08-19 20:11:31 -0500

    add heroku configuration

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 const whitelist = ['http://localhost:8080', 'https://myapp.com', 'http://127.0.0.1:5500'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('No permitido'));
